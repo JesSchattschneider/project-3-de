be29d9ab43a4470476280f45983dc7cde76f0c4b
JesSchattschneider <jessica.leiria@gmail.com>
2024-08-12 15:04:07 +1200

    add: refactor manifest path

--- analytics/assets/dbt/dbt.py
+++ analytics/assets/dbt/dbt.py
@@ -6,19 +6,25 @@ from dagster import AssetExecutionContext
 # Corrected path to point to the correct dbt project directory
 dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "..", "warehouse_snowflake").resolve()
 print(f"dbt_project_dir: {dbt_project_dir}")
-
-# Initialize the DbtCliResource with the dbt project directory
 dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
-# Path to the manifest.json file in the dbt target directory
-dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
+# generate manifest
+dbt_manifest_path = (
+    dbt_warehouse_resource.cli(
+        ["--quiet", "parse"],
+        target_path=Path("target"),
+    )
+    .wait()
+    .target_path.joinpath("manifest.json")
+)
+
 print(f"dbt_manifest_path: {dbt_manifest_path}")
 
 # Check if the manifest.json file exists and raise an error if not
 if not dbt_manifest_path.exists():
     raise FileNotFoundError(f"manifest.json does not exist at {dbt_manifest_path}. Please check your dbt run.")
 
-# Load the manifest to produce asset definitions
+# load manifest to produce asset defintion
 @dbt_assets(manifest=dbt_manifest_path)
 def dbt_warehouse(context: AssetExecutionContext, dbt_warehouse_resource: DbtCliResource):
     yield from dbt_warehouse_resource.cli(["run"], context=context).stream()
