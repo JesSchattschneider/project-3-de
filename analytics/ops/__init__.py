357e223babfc7c0e26932e19df9f6c9ac9554b0b
JesSchattschneider <jessica.leiria@gmail.com>
2024-08-13 12:14:47 +1200

    fix: data ingestion and remove extra debugging

--- analytics/ops/__init__.py
+++ analytics/ops/__init__.py
@@ -1,13 +1,8 @@
-from dagster import op, OpExecutionContext
+from dagster import op
 import xml.etree.ElementTree as ET
 import pandas as pd
-import datetime
-from sqlalchemy import Table, MetaData, Column, String, Integer, Float, DateTime
-from sqlalchemy import create_engine, MetaData, Table, text
-from sqlalchemy.engine.url import URL
-from sqlalchemy.exc import SQLAlchemyError
-from typing import List, Dict, Optional, Any
-import numpy as np
+from typing import Optional, Any
+
 # Sample data handling function
 def parse_wfs_data(data: dict) -> pd.DataFrame:
     """Parses the wfs data."""
@@ -33,7 +28,6 @@ def get_one(context):
             # select from dim_temperature table where temperature = 11.83 limit 1
             cursor.execute("SELECT * FROM dim_temperature WHERE temperature = 11.83 LIMIT 1")
             result = cursor.fetchone()
-            print(result)
             context.log.info(f"Result: {result}")
 
 def _create_table_if_not_exists(cursor, table_name, df, logger, datetime_columns=None):
@@ -55,7 +49,6 @@ def _create_table_if_not_exists(cursor, table_name, df, logger, datetime_columns
             create_stmt = f"CREATE TABLE {table_name} ({column_definitions})"
             cursor.execute(create_stmt)
             logger.info(f"Table {table_name} created.")
-            print(f"Table {table_name} created.")
 
 def _insert_data(cursor, table_name, data_to_insert):
         columns = data_to_insert[0].keys()
@@ -68,9 +61,24 @@ def _insert_data(cursor, table_name, data_to_insert):
         for row in data_to_insert:
             cursor.execute(insert_stmt, row)
 
+def _replace_symbols(value):
+    if isinstance(value, str):
+        # Replace "<" with "less_than_" and ">" with "greater_than_"
+        if value.startswith('<'):
+            return f"less_than_{value[1:]}"
+        elif value.startswith('>'):
+            return f"greater_than_{value[1:]}"
+    return value
+
 def _upsert_data(cursor, table_name, data_to_insert, primary_key):
     if not data_to_insert:
         return  # Exit if there's no data to insert
+    
+    # Replace symbols in the 'value' column and convert all values to string
+    for row in data_to_insert:
+        if 'value' in row:
+            row['value'] = _replace_symbols(row['value'])
+            row['value'] = str(row['value'])        
 
     # Extract columns from data
     columns = data_to_insert[0].keys()
@@ -93,10 +101,7 @@ def _upsert_data(cursor, table_name, data_to_insert, primary_key):
         WHERE target.{primary_key} = source.{primary_key}
     );
     """
-    
-    # Print SQL statement for debugging
-    print(f"Executing SQL: {insert_stmt}")
-    
+
     try:
         # Execute the INSERT statement
         cursor.execute(insert_stmt)
@@ -169,51 +174,12 @@ def load_data_to_snowflake(snowflake_resource_con: Any,
             with snowflake_resource_con.get_connection() as conn:
                 with conn.cursor() as cursor:
                     # Insert into main table
-                    print(df.columns)
                     _create_table_if_not_exists(cursor, table_name, df, logger)
-                    
-                    test = df.head(2)
-                    
-                    data_to_upsert = test.to_dict(orient='records')
+                                        
+                    data_to_upsert = df.to_dict(orient='records')
                     _upsert_data(cursor, table_name, data_to_upsert, primary_key = 'id')
                     
                     logger.info("Data successfully inserted into the database and latest table.")
         
         except Exception as e:
             logger.error(f"Failed to upsert to database: {e}")
-
-
-@op(required_resource_keys={"snowflake_resource"})
-def wfs_db_table(context: OpExecutionContext, 
-                        transformed_df: pd.DataFrame, vars: list[str]) -> None:
-    """Produces wfs table and upserts to Snowflake.
-
-    Args:
-        snowflake_conn: a SnowflakeResource object
-        transformed_df: the transformed DataFrame
-        vars: list of column names
-    """
-
-    # Add an additional column for the primary key
-    transformed_df["pk_lawa_and_council_id"] = transformed_df["lawasiteid"].astype(str) + "__" + transformed_df["councilsiteid"].astype(str)
-    # Add partition_key columns
-    transformed_df['partition_key'] = context.partition_key
-
-    # Transform the DataFrame to a list of dictionaries
-    data_to_db = transformed_df.to_dict(orient="records") 
-
-    # Define table metadata
-    metadata = MetaData()
-    table = Table(
-        "wfs_table",
-        metadata,
-        Column("pk_lawa_and_council_id", String, primary_key=True),
-        *(Column(var, String) for var in vars),
-        Column("partition_key", String)
-    )
-
-    print(table)
-
-    # Upsert data to Snowflake
-    # append_to_database(context, snowflake_conn, data_to_db, table, metadata)
-    
